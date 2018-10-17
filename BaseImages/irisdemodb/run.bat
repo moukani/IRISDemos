9e7be0a5ee67d220207e8eb2856b7df96311c402
Amir Samary <amirsamary@gmail.com>
2018-10-17 15:20:32 -0400

    Use key on IRISLicense

--- BaseImages/irisdemodb/run.bat
+++ BaseImages/irisdemodb/run.bat
@@ -5,4 +5,4 @@ set CONTAINER_NAME=%CurrDirName%
 
 set IMAGE_NAME=amirsamary/irisdemo:%CONTAINER_NAME%
 
-docker run --rm -it -p 51773:51773 -p 52773:52773 -v %cd%\shared:/shared --name %CONTAINER_NAME% %IMAGE_NAME% --key /shared/iris.key
+docker run --rm -it -p 51773:51773 -p 52773:52773 -v %cd%\shared:/shared -v %cd%\..\..\\IRISLicense:/irislicense --name %CONTAINER_NAME% %IMAGE_NAME% --key /irislicense/iris.key
