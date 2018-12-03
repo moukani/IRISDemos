8a5a7ddac29f5e6e4ccc58b5b40c377da37edac5
Amir Samary <amirsamary@gmail.com>
2018-12-03 10:37:02 -0500

    Fixing iKnow

--- ShellScriptUtils/downloadimg.sh
+++ ShellScriptUtils/downloadimg.sh
@@ -30,6 +30,15 @@ do
     fi
 done
 
+printfY "\n\nPulling 2018.1.1 image so we can extract iKnow related resources from it...\n"
+docker pull docker.iscinternal.com/intersystems/iris:2018.1.1-stable
+if [ $? -eq 0 ]; then 
+    printfG "\nPull of docker.iscinternal.com/intersystems/iris:2018.1.1-stable succesful. \n"
+else
+    printfR "\nPull of docker.iscinternal.com/intersystems/iris:2018.1.1-stable failed. \n"
+    exit 0
+fi
+
 printfY "\n\nEnter with your credentials on docker hub so we can upload the images:\n"
 dockerLogin
 
@@ -45,6 +54,14 @@ do
     fi
 done
 
+docker tag docker.iscinternal.com/intersystems/iris:2018.1.1-stable amirsamary/irisdemo:iris.2018.1.1-stable
+if [ $? -eq 0 ]; then 
+    printfG "\Tagging of docker.iscinternal.com/intersystems/iris:2018.1.1-stable as amirsamary/irisdemo:iris.2018.1.1-stable successful\n"
+else
+    printfR "\Tagging of docker.iscinternal.com/intersystems/iris:2018.1.1-stable as amirsamary/irisdemo:iris.2018.1.1-stable failed\n"
+    exit 0
+fi
+
 printfY "\n\Uploading images...\n"
 for image in icm iris arbiter webgateway;
 do
@@ -56,3 +73,11 @@ do
         exit 0
     fi
 done
+
+docker push amirsamary/irisdemo:iris.2018.1.1-stable
+if [ $? -eq 0 ]; then 
+    printfG "\Pushing of amirsamary/irisdemo:iris.2018.1.1-stable successful.\n"
+else
+    printfR "\Pushing of amirsamary/irisdemo:iris.2018.1.1-stable successful.\n"
+    exit 0
+fi
