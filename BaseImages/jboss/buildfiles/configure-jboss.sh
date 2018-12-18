5b6cee8a01cbf6859f1083a20522bbb988edef47
Amir Samary <amirsamary@gmail.com>
2018-12-18 13:40:29 -0500

    Configuring JDBC driver on JBoss without IRISDataSource. Giving a hostname to callcenterui container

--- BaseImages/jboss/buildfiles/configure-jboss.sh
+++ BaseImages/jboss/buildfiles/configure-jboss.sh
@@ -2,6 +2,10 @@
 # Author: Amir Samary
 # Taken and adapted from https://goldmann.pl/blog/2014/07/23/customizing-the-configuration-of-the-wildfly-docker-image/
 #
+# Validate if we should use IRISDataSource instead of IRISDriver. Removed command:
+# /subsystem=datasources/jdbc-driver=com.intersystems.jdbc.IRISDriver:add(driver-name=com.intersystems.jdbc.IRISDriver,driver-datasource-class-name=com.intersystems.jdbc.IRISDataSource,driver-module-name=com.intersystems)
+# from configure-jboss.cli.
+#
 JBOSS_CLI=$JBOSS_HOME/bin/jboss-cli.sh
 JBOSS_MODE=${1:-"standalone"}
 JBOSS_CONFIG=${2:-"$JBOSS_MODE.xml"}
