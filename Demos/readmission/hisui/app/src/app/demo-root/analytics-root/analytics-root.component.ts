5bd6e0dcb5bbc7862505b669d1c047941dad6ed5
Phillip Booth <phillip.booth2015@gmail.com>
2019-01-17 15:43:23 -0500

    adding analytics tab, fixing date formatting, and adding width to admission  number

---
+++ Demos/readmission/hisui/app/src/app/demo-root/analytics-root/analytics-root.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-analytics-root',
+  templateUrl: './analytics-root.component.html',
+  styleUrls: ['./analytics-root.component.css']
+})
+export class AnalyticsRootComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
