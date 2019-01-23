5b346b33fc3357502381a15c02746e9326a6d359
Amir Samary <amirsamary@gmail.com>
2019-01-22 23:18:23 -0500

    Using sudo to delete files generated by zeppelin image when rebuilding the demo

--- Demos/readmission/build.sh
+++ Demos/readmission/build.sh
@@ -12,6 +12,6 @@ git submodule update
 printfY "\nBUILDING...\n"
 docker-compose stop
 docker-compose rm -f
-rm -rf ./advanced_analytics/shared/zeppelin/conf
-rm -rf ./advanced_analytics/shared/zeppelin/logs
+sudo rm -rf ./advanced_analytics/shared/zeppelin/conf
+sudo rm -rf ./advanced_analytics/shared/zeppelin/logs
 docker-compose build
