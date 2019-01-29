d7280a66ac58e85ff19bdb9e31d18637bc273266
Amir Samary <amirsamary@gmail.com>
2019-01-29 18:10:00 -0500

    Better messages

--- Demos/readmission/build.sh
+++ Demos/readmission/build.sh
@@ -9,9 +9,11 @@ printfY "\nUpdating sub modules...\n"
 git submodule init
 git submodule update
 
-printfY "\nBUILDING...\n"
+printfY "\nStopping and removing old containers...\n"
 docker-compose stop
 docker-compose rm -f
+printfY "\nRemoving zeppelin configuration (you may be required to enter with your sudo password)...\n"
 sudo rm -rf ./advanced_analytics/shared/zeppelin/conf
 sudo rm -rf ./advanced_analytics/shared/zeppelin/logs
+printfY "\nBuilding...\n"
 docker-compose build
