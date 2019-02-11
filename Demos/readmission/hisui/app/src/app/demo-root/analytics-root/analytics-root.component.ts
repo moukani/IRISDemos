6c0ee9d2c2f0e00fdab6d5e18d8cbf2fff9d1981
Phillip Booth <phillip.booth2015@gmail.com>
2019-02-11 16:18:14 -0500

    adding fix for iframe url

--- Demos/readmission/hisui/app/src/app/demo-root/analytics-root/analytics-root.component.ts
+++ Demos/readmission/hisui/app/src/app/demo-root/analytics-root/analytics-root.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-analytics-root',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalyticsRootComponent implements OnInit {
 
-  constructor() { }
+  iframe_src: any = "";
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.iframe_src = sanitizer.bypassSecurityTrustResourceUrl("http://" + window.location.hostname + ":9094/csp/appint/_DeepSee.UserPortal.DashboardViewer.zen?DASHBOARD=LACE/LACE Scores.dashboard&EMBED=1&IRISUserName=SuperUser&IRISPassword=sys")
+  }
 
   ngOnInit() {
   }
